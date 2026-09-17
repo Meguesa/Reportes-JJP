ecef198631e3a46df9d3f5ed8b550dca257eed56
gguerra92 <77221428+gguerra92@users.noreply.github.com>
2026-09-17 11:11:38 -0600

    Agregar configuración centralizada de SharePoint

---
+++ config.js
@@ -0,0 +1,11 @@
+window.REPORTES_JJP_CONFIG = Object.freeze({
+  appName: "Reportes JJP",
+  sharePoint: {
+    hostname: "meguesajdjp.sharepoint.com",
+    sitePath: "/sites/CentroControlDireccion",
+    siteName: "Centro de Control Dirección",
+    listName: "Reportes",
+    listUrl: "https://meguesajdjp.sharepoint.com/sites/CentroControlDireccion/Lists/Reportes/AllItems.aspx"
+  },
+  areas: ["Vendedores", "Parque", "Capillas", "Administradores"]
+});
