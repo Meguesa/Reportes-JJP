9af9b6fbb8453bac9e0fa09a8762e4dfa4990496
gguerra92 <77221428+gguerra92@users.noreply.github.com>
2026-09-17 11:31:16 -0600

    Configurar Microsoft Entra para Reportes JJP

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
-window.REPORTES_JJP_CONFIG = Object.freeze({
+export const REPORTES_JJP_CONFIG = Object.freeze({
   appName: "Reportes JJP",
+  auth: {
+    clientId: "78813e75-d4b0-4dd4-868b-8b7e9d6d94a7",
+    tenantId: "888d54c0-f785-49d1-b967-54da8b0aed94",
+    authority: "https://login.microsoftonline.com/888d54c0-f785-49d1-b967-54da8b0aed94",
+    redirectUri: "https://meguesa.github.io/Reportes-JJP/auth-redirect.html",
+    loginScopes: ["openid", "profile", "email"]
+  },
   sharePoint: {
     hostname: "meguesajdjp.sharepoint.com",
     sitePath: "/sites/CentroControlDireccion",
